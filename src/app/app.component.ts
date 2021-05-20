420c9159cccdae014367ba2e7f8cddb9d6ce6148
Mohammad <Farah_Mohammad@Dell.com>
2021-05-20 22:41:07 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template: `
+  //         <h1>Hello from Template</h1>
+  // `,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // providers: [ DataService]
 })
 export class AppComponent {
   title = 'my-shiny-new-angular-app';
+  name:string="Yuni";
+  id:number=100;
+  age:number=23;
+  result:boolean=true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { TransformDirective } from './transform.directive';
+import { SafePipe } from './pipes/safe/safe.pipe';
+import { HeaderComponent } from './core/header/header.component';
+import { CoreModule } from './core/core.module';
+import { ChatComponent } from './components/chat/chat.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
+  // declaration only accept Component, Directives and Pipe 
+  // but not Services or Module
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    UserComponent,
+    TransformDirective,
+    SafePipe,
+    ChatComponent,
+    UsersComponent,
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule 
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, UserComponent]
 })
 export class AppModule { }
